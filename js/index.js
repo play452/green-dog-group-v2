fd9129aa89ba1aa6a91f0a4f65c5656b187da440
play452 <oscarlin@edu.schools.us.kg>
2024-08-18 16:47:35 -0400

    update

--- js/index.js
+++ js/index.js
@@ -23,22 +23,4 @@ const error = document.getElementById("uv-error");
 /**
  * @type {HTMLPreElement}
  */
-const errorCode = document.getElementById("uv-error-code");
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  try {
-    await registerSW();
-  } catch (err) {
-    error.textContent = "Failed to register service worker.";
-    errorCode.textContent = err.toString();
-    throw err;
-  }
-
-  const url = search(address.value, searchEngine.value);
-
-  let frame = document.getElementById("uv-frame");
-  frame.style.display = "block";
-  frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
-});
